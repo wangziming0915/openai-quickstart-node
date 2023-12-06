8e4dbfe46b04ae96bdc7c7db3e5c64f8e8d8ea12
Dylan_Wong <wzmsilver5@gmail.com>
2023-12-05 21:43:07 -0800

    getResuilt.js implemented

--- pages/index.js
+++ pages/index.js
@@ -40,16 +40,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Get your answer</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Enter your question"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate Answers" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
